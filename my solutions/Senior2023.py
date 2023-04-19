62de0085c91d3636d8a51d34731b5b034f9f4974
szjstar <73240804+szjstar@users.noreply.github.com>
2023-04-19 21:30:49 +0800

    working on senior 2023 problem
    I think that I misunderstand the problem a little ....

--- my solutions/Senior2023.py
+++ my solutions/Senior2023.py
@@ -1,19 +1,32 @@
 """ read the inputs"""
 c = int(input())  # the number of pairs of triangles
 
-line1, line2 = [], []  # the two lines of triangles
+row1, row2 = [], []  # the two rows of triangles
 tmp = input().split()
 for i in tmp:
     if i == "1":
-        line1.append(True)
+        row1.append(True)
     else:
-        line1.append(False)
+        row1.append(False)
 
 tmp = input().split()
 for i in tmp:
     if i == "1":
-        line2.append(True)
+        row2.append(True)
     else:
-        line2.append(False)
+        row2.append(False)
 
 """ the solution"""
+ans = 0
+for i in range(c):  # go through all the colums
+    if i % 2:  # if the two triangles in the colum are laid top-to-top
+        # no mater what, the bottom sides of the two triangles are highlighted whenever they are wet
+        ans += row1[i] + row2[i]
+        # the left sides of the upper and lower triangles are highlighted whenever they are wet and the sector before(on the left of)  them are dry (else then they are already marked)
+        ans += (row1[i] and (not row1[i-1])) + (row2[i] and (not row2[i-1]))
+        # the right sides of the upper and lower triangles are highlighted whenever they are wet (we don't need to care about righter triangles because they will be counted later)
+        ans += row1[i] + row2[i] 
+    
+    else:  # if the two triangles in the colum are laid bottom-by-bottom
+        #  the side between the upper and lower triangles is highlighted
+        ans += row1[i] or row2[i]
