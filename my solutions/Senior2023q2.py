24202952d51cb39d8dcbb69386b505ea33feefd1
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-05-12 11:55:00 +0800

    finished senior2023 question 2, waiting for test

--- my solutions/Senior2023q2.py
+++ my solutions/Senior2023q2.py
@@ -9,11 +9,15 @@ for i in tmp:
     
 def find_asymmetric_value(heights):
     length = len(heights)
+    asymmetric = 0
     for i in range(int(length/2)):
-        
+        asymmetric += abs(heights[i] - heights[-1-i])
+    return asymmetric
     
 for length in range(1,n+1):
+    min_asymmetric = float("inf")
     for start in range(n-length):
         end = start+length
         h1 = cp(h[start:end])
-        
+        min_asymmetric = min(find_asymmetric(h1), min_asymmetric)
+    print(min_asymmetric,end=" ")
