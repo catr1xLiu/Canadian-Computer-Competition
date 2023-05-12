1538a5b8965050e6eaf1dfdd630496fb146e8a5b
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-05-12 11:44:13 +0800

    working  on Senior2023 question 2

--- my solutions/Senior2023q2.py
+++ my solutions/Senior2023q2.py
@@ -1,1 +1,18 @@
 
+n = int(input())
+tmp = input.split()
+h=[]
+for i in tmp:
+    h.append(int(i))
+from copy import deepcopy as cp
+
+n = int(input())
+tmp = input.split()
+h=[]
+for i in tmp:
+    h.append(int(i))
+
+for length in range(1,n+1):
+    for start in range(n-length):
+        end = start+length
+        tmp = cp(h[start:end])
