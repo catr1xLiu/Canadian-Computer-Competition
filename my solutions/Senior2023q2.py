52785dc857a6927f64318b1d24547f07386a1b5a
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-05-12 11:49:08 +0800

    Update Senior2023q2

--- my solutions/Senior2023q2.py
+++ my solutions/Senior2023q2.py
@@ -1,9 +1,3 @@
-
-n = int(input())
-tmp = input.split()
-h=[]
-for i in tmp:
-    h.append(int(i))
 from copy import deepcopy as cp
 
 n = int(input())
@@ -12,7 +6,14 @@ h=[]
 for i in tmp:
     h.append(int(i))
 
+    
+def find_asymmetric_value(heights):
+    length = len(heights)
+    for i in range(int(length/2)):
+        
+    
 for length in range(1,n+1):
     for start in range(n-length):
         end = start+length
-        tmp = cp(h[start:end])
+        h1 = cp(h[start:end])
+        
