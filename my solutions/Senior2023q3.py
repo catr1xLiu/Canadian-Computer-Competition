963218c01c7531735699f2d35e889d9712adebbb
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-05-30 20:56:32 +0800

    finished 2023 q3's program
    To be debugged...

--- my solutions/Senior2023q3.py
+++ my solutions/Senior2023q3.py
@@ -1,8 +1,10 @@
 # solution for senior 2023 question 3
 
+from copy import deepcopy as cp
+
 class Grid:
   nums = [] # nums[row][colum]
-  colums_asymmetry= [] # 1 for not symmetric
+  colums_asymmetry = [] # 1 for not symmetric
   rows_asymmetry = []
   def __init__(n, m): # n and m is the number of rows and colums
     self.nums = [[0 for i in range(m)] for j in range(n)]
@@ -34,7 +36,28 @@ class Grid:
     result = 0
     for i in self.rows_asymmetry:
       result += i
+    return 
+  
+  def get_number_of_asymmetric_colums():
+    result = 0
+    for i in self.colums_asymmetry:
+      result += i
     return result
+    
+  def copy():
+    copied_self = Grid
+    copied_self.nums = cp(self.nums)
+    copied_self.rows_asymmetry = cp(self.rows_asymmetry)
+    copied_self.colums_asymmetry = cp(self.colums_asymmetry)
+    return copied_self
+    
+  def __str__:
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    string = ""
+    for i in range(len(rows_asymmetry)):
+      for j in range(len(colums_asymmetry)):
+        string += alphabet[i][j]
+      string += "\n"
 
 n, m, r, c = input().split()
 n = int(n)
@@ -44,5 +67,43 @@ c = int(c)
 
 grid = Grid(n, m)
 
+ans = "IMPOSSIBLE"
+found = False
+
 def dfs(grid:Grid):
+  if grid.get_number_of_asymmetric_rows == r and grid.get_number_of_asymmetric_colums == c:
+    ans = grid
+    found = True
+    return
   
+  if found:
+    return
+  
+  # go through all the options we can do
+  options_both_closing = [] # the options that can make the grid have both r and c closer to the targeted
+  options_rows_closing = [] # the options that can make the amount of asymmetric rows approaching the objective quantity
+  options_colums_closing = [] # the options for colums
+  # TODO better sort all the options of best to worse in the order of how good it is leading to the required r and c
+  for i in range(len(n)):
+    for j in range(len(m)):
+      option = grid.copy()
+      option[i][j] += 1
+      rows_closing = (option.get_number_of_asymmetric_rows() - grid.get_number_of_asymmetric_rows) * (r - grid.get_number_of_asymmetric_rows) > 0 # if we are moving in the right direction in the matter of the amount of asymmetric rpws required
+      colums_closing = (option.get_number_of_asymmetric_colums() - grid.get_number_of_asymmetric_colums) * (c - grid.get_number_of_asymmetric_colums) > 0 # in the matter of colums
+      if rows_closing:
+        if colums_closing:
+          options_both_closing.append(option)
+        else:
+          options_rows_closing.append(option)
+      else if colums_closing:
+        options_colums_closing.append(option)
+  
+  for option in options_both_closing:
+    dfs(option)
+  for option in options_colums_closing:
+    dfs(option)
+  for option in options_rows_closing:
+    dfs(option)
+    
+dfs(grid)
+print(ans)
