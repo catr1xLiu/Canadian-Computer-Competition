f6190efa349e5d97db6fc3b1ea4c7fd05db24338
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-12-05 10:22:01 +0800

    working on 2021 p4

--- my solutions/simulate tests/2023/2021P4.py
+++ my solutions/simulate tests/2023/2021P4.py
@@ -1,3 +1,19 @@
-n, w, d = map(int, input())
+n, w, d = map(int, input().split())
 
-connection = []
+connection = {}
+for i in range(w):
+    a, b = map(int, input().split())
+    start = min(a,b)-1
+    end = max(a,b)-1
+    try:
+        connection[start].append(end)  # we change every point to 0~n-1
+    except KeyError:
+        connection[start] = [end]
+
+time_arrival_results = [[-1 for i in range(n)] for j in range(n)] #[pos, t] = time_arrival
+def time_arrival(pos, t):
+    if pos == n:
+        return t
+    if t > n:
+        return n # the maximum amount of time needed
+    if time_arrival[pos][t]
