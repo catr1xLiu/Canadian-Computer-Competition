7baf02cc6f07c41fd2827ad2540d58f91fba7e01
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-12-17 20:41:27 +0800

    working on attempt

--- my solutions/ACSL formal competition/attempt2.py
+++ my solutions/ACSL formal competition/attempt2.py
@@ -6,10 +6,12 @@ for i in inp:
 tmp = input().split()
 hand = []
 for i in tmp:
+    i = "0" * (2-len(i)) + i 
     hand.append((int(i[0]), int(i[1])))
 tmp = input().split()
 draw = []
 for i in tmp:
+    i = "0" * (2-len(i)) + i 
     draw.append((int(i[0]), int(i[1])))
 
 def match_tile(tile, row):
@@ -35,54 +37,60 @@ def is_double(row):
     
     return rows[row][-1][0] == rows[row][-1][1]
 
-previously_touched_row = 0
+previously_touched_row = -1
 def obsorb_draw_tile():
+    if len(draw) == 0:
+        return False
     hand.append(draw.pop(0))
+    return True
 
 def print_rows():
     for i in rows:
         print(i)
     print()
 
-while len(draw):
-    rows_placed_this_round = []
+
+while True:
+    # print(hand)
+    # print(previously_touched_row)
+    # print_rows()
     flag = False
-    if is_double(previously_touched_row):
+    if previously_touched_row != -1 and is_double(previously_touched_row):
         for tile_id in range(len(hand)): # go through all tiles in hand
             match = match_tile(hand[tile_id], previously_touched_row)
             if match != -1:
                 rows[previously_touched_row].append(match)
-                hand.pop(previously_touched_row)
+                hand[tile_id] = -1
                 flag = True
-
+                break
     else:
         for tile_id in range(len(hand)): # go through all tiles in hand
-            print(hand[tile_id])
-            print(rows_placed_this_round)
-            print_rows()
-
+            if flag:
+                break
             match = match_tile(hand[tile_id], (previously_touched_row+1)%4)
-            if (match != -1) and (previously_touched_row+1 not in rows_placed_this_round):
-                rows[previously_touched_row].append(match)
-                rows_placed_this_round.append(previously_touched_row)
-                hand[previously_touched_row] = -1
-                flag = True
+            if (match != -1):
+                rows[(previously_touched_row+1)%4].append(match)
+                hand[tile_id] = -1
+                flag=True
+                break
             for row in range(4):
                 # print(hand,i)
                 match = match_tile(hand[tile_id], row)
-                if (match != -1) and (row not in rows_placed_this_round):
+                if match != -1:
                     rows[row].append(match)
-                    rows_placed_this_round.append(row)
                     hand[tile_id] = -1
                     previously_touched_row = row
                     flag = True
-
+                    break
     if not flag:
-        obsorb_draw_tile()
+        if not obsorb_draw_tile():
+            break
 
-print(hand)
-
-
+sum = 0
+for i in hand:
+    if i != -1:
+        sum += i[0] + i[1]
+print(sum)
 
 ''' 
 notice, for each pile on our hand, we are asked to examine whether it can be appended to any of the rows, starting with the one AFTER the one to which the last pile is appended to 
