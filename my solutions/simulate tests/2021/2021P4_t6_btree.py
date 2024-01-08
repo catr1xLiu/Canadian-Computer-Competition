7735752ffed99937e329e970ca5aa662f3df482c
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2024-01-08 15:46:55 +0800

    copid it to 2021p4t6

--- my solutions/simulate tests/2021/2021P4_t6_btree.py
+++ my solutions/simulate tests/2021/2021P4_t6_btree.py
@@ -4,6 +4,78 @@ so no need to worry about when we can get on the train
 this should be very easy
 '''
 
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+    def __str__(self, step=0) -> str:
+        string = f"node with value:{self.value}"
+        if self.left is not None:
+            string += f", left-brench:{self.left.__str__(step+1)}"
+        if self.right is not None:
+            string += f", right-brench:{self.right.__str__(step+1)}"
+        return "\n" + "   "*step + "{\n}" + "   "*step + string + "\n" +  "   "*step + "}\n"
+    
+def create_btree(list_sorted):
+    if len(list_sorted) == 1:
+        return Node(list_sorted[0])
+    if len(list_sorted) == 2:
+        return Node(list_sorted[0], None, Node(list_sorted[1]))
+    mid = (len(list_sorted)) // 2
+
+    return Node(list_sorted[mid], create_btree(list_sorted[:mid]), create_btree(list_sorted[mid+1:]))
+
+def add_node(tree:Node, newNode:Node, values_map):
+    if values_map[newNode.value] > values_map[tree.value]:
+        if tree.right is None:
+            tree.right = newNode
+        else:
+            add_node(tree.right, newNode, values_map)
+    elif values_map[newNode.value] < values_map[tree.value]:
+        if tree.left is None:
+            tree.left = newNode
+        else:
+            add_node(tree.left, newNode, values_map)
+
+def find_path(tree:Node, value, values_map, path=[]):
+    '''
+    get the path, in a list
+    0 means left, 1 means right
+    -1 for unfound
+    '''
+    if value > values_map[tree.value]:
+        if tree.right is None:
+            return -1
+        return find_path(tree.right, value, values_map, path=path + [1])
+    if value < values_map[tree.value]:
+        if tree.left is None:
+            return -1
+        return find_path(tree.left, value, values_map, path=path+[0])
+    if tree.value == value:
+        return path
+
+def delete_node(tree:Node, path)->Node:
+    # None if path invalid
+    if path == -1 or len(path)==0:
+        return None
+    if len(path) == 1:
+        if path[0]:
+            swap = tree.right
+            tree.right = None
+            return swap
+        swap = tree.left
+        tree.left = None
+        return swap
+    
+    if path[0]:
+        return delete_node(tree.right, path[1:])
+    return delete_node(tree.left, path[1:])
+
+
+
+
 from time import perf_counter as us
 
 n, w, d = map(int, input().split())
@@ -86,21 +158,7 @@ def mergesort(indexs, values:dict):
 
 station_transfer_in_increasing_time_order = mergesort(station_transfer_in_increasing_time_order, time_needed_to_arrive_through_station_transfer)
 
-def b_search_index(value, indexes_sorted:list, values:dict, left_bound=0, rightbound=-1):
-    if rightbound == -1:
-        rightbound = len(indexes_sorted)
 
-    if (rightbound - left_bound == 0):
-        return 0
-    if (rightbound - left_bound == 1):
-        if value < values[indexes_sorted[left_bound]]:
-            return left_bound
-        return left_bound + 1
-    
-    mid = (left_bound + rightbound) // 2
-    if value < values[indexes_sorted[mid]]:
-        return b_search_index(value, indexes_sorted, values, left_bound=left_bound, rightbound=mid)
-    return b_search_index(value, indexes_sorted, values, left_bound=mid, rightbound=rightbound)
 
 def update_time_needed_arrive_through_transfer(station):
     if station not in station_transfer_in_increasing_time_order:
